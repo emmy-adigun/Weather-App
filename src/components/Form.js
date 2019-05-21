4d0e81d18f6b4eea67ab7588d4c9e50115a09630
Emmanuel Adigun <Emmanuel.Adigun@Interswitchng.com>
2019-05-21 17:29:39 +0100

    new updates

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,8 +4,7 @@ export default class Form extends Component{
   render(){
     return(
       <div>
-        <h2>Weather Information</h2>
-        <p>Form Component</p>
+
         <form onSubmit={this.props.getWeather}>
           <input type="text" name="city" placeholder="city" />
           <input type="text" name="country" placeholder="country" />
